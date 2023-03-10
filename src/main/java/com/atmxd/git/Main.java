7680a6e431537ca16436dd6afa8d65c95d49804e
mxd <2739973102@qq.com>
2023-03-11 00:50:11 +0800

    second commit

--- src/main/java/com/atmxd/git/Main.java
+++ src/main/java/com/atmxd/git/Main.java
@@ -2,6 +2,8 @@ package com.atmxd.git;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
