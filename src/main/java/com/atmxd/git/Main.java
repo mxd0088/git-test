45b5a57e921ce1225273d8f0bc00648f578156ef
mxd <2739973102@qq.com>
2023-06-19 11:55:27 +0800

    push commit7

--- src/main/java/com/atmxd/git/Main.java
+++ src/main/java/com/atmxd/git/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("pull test!");
         System.out.println("pull test!");
         System.out.println("pull test!");
+        System.out.println("pull test!");
     }
 }
